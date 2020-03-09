6c3ccb3fe45136149a63d0efb4d19d1cb55758c8
andrepp97 <andreputerap@gmail.com>
2020-03-09 22:13:33 +0700

    switch DB

--- database/index.js
+++ database/index.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql')
 
 const conn = mysql.createConnection({
-    // host: 'db4free.net',
-    // port: 3306,
-    // user: 'sineaskolektif',
-    // password: 'qwerty321',
-    // database: 'sineaskolektif',
-    // timezone: 'UTC'
-
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sineas_kolektif',
+    host: 'db4free.net',
+    port: 3306,
+    user: 'sineaskolektif',
+    password: 'qwerty321',
+    database: 'sineaskolektif',
     timezone: 'UTC'
+
+    // host: 'localhost',
+    // user: 'root',
+    // password: '',
+    // database: 'sineas_kolektif',
+    // timezone: 'UTC'
 })
 
 module.exports = {
