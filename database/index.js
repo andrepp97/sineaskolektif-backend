08db516f1c4270514c61db730d122717ae0c8742
andrepp97 <andreputerap@gmail.com>
2020-03-14 13:42:10 +0700

    switch db

--- database/index.js
+++ database/index.js
@@ -9,6 +9,7 @@ const conn = mysql.createConnection({
     timezone: 'UTC'
 
     // host: 'localhost',
+    // port: 3306,
     // user: 'root',
     // password: '',
     // database: 'sineas_kolektif',
